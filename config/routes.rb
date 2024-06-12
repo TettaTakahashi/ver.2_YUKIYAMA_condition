019197ef1ff3f3e965dac178f95aba3fe4b19ec3
TettaTakahashi <x4tetta@gmail.com>
2024-06-12 13:50:56 +0000

    [Update]devise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :owners
-  devise_for :customers
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :owners, skip: [:passwords], controllers: {
+    registrations: "owner/registrations",
+    sessions: 'owner/sessions'
+  }
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
