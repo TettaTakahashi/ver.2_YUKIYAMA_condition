1dc91eb1fb0d29c8d90d4b00eabd2f89c52e8fb8
TettaTakahashi <x4tetta@gmail.com>
2024-06-14 14:13:39 +0000

    [Update]admin情報追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Admin.find_or_create_by!(email: 'aaa@aaa') do |admin|
+  admin.email = "aaa@aaa"
+  admin.password = "#{ENV["SECRET_KEY"]}"
+end
