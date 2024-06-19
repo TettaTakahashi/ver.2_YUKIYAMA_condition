c8b7ef07d582bc28ba112998490c1b6945313295
TettaTakahashi <x4tetta@gmail.com>
2024-06-19 13:27:57 +0000

    [Update]新規登録、ログインTOPページ追加、背景挿入

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,10 @@ class HomesController < ApplicationController
   def top
   end
   
+  def registration_top
+  end
+  
+  def session_top
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
+  get 'homes/registration_top'
+  get 'homes/session_top'
 end
