6b3f71c51e092e5c0f4a4d4555fdfd058c462c2e
TettaTakahashi <x4tetta@gmail.com>
2024-06-26 06:03:23 +0000

    [Update]都道府県追加機能

--- app/controllers/admin/prefectures_controller.rb
+++ app/controllers/admin/prefectures_controller.rb
@@ -3,6 +3,27 @@ class Admin::PrefecturesController < ApplicationController
   
   
   def index
-      
+    @prefectures = Prefecture.all
+    @prefecture = Prefecture.new
   end
+  
+  
+  def create
+    @prefecture = Prefecture.new(prefecture_params)
+    if @prefecture.save
+      redirect_to admin_prefectures_path
+    else
+      @prefectures=Prefecture.all
+      redirect_to admin_prefectures_path
+    end
+  end
+  
+  
+  
+  private
+  
+  def prefecture_params
+    params.require(:prefecture).permit(:prefecture_name)
+  end
+  
 end
