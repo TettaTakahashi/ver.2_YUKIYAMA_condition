762b51ae370c80de34ad49f076fb3b2dedcb28be
TettaTakahashi <x4tetta@gmail.com>
2024-06-28 11:22:38 +0000

    [Update]利用者一覧ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,4 +5,9 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page]).per(20)
   end
+  
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,7 +27,7 @@ class Admin::SessionsController < Devise::SessionsController
   
   
   def after_sign_in_path_for(resource)
-    root_path
+    admin_prefectures_path
   end
   
   def after_sign_out_path_for(resource)
