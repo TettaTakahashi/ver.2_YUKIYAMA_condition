2f144cdf666ef592974d364fd1d95b16edca6269
TettaTakahashi <x4tetta@gmail.com>
2024-07-01 12:53:01 +0000

    [Update]admin経営者編集ページ

--- app/controllers/admin/owners_controller.rb
+++ app/controllers/admin/owners_controller.rb
@@ -10,4 +10,24 @@ class Admin::OwnersController < ApplicationController
   def show
     @owner = Owner.find(params[:id])
   end
+  
+  
+  def edit
+    @owner = Owner.find(params[:id])
+  end
+  
+  
+  def update
+    @owner = Owner.find(params[:id])
+    @owner.update(owner_params)
+    redirect_to admin_owner_path(@owner)
+  end
+  
+  
+  
+  private
+  
+  def owner_params
+    params.require(:owner).permit(:owner_name, :email)
+  end
 end
